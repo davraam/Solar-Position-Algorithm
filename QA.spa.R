30d20133105345af3f21fc8fbef9122ec5135104
Demetris Avraam <avraam_dimitris@hotmail.com>
2017-07-27 09:51:30 +0300

    Add files via upload

--- QA.spa.R
+++ QA.spa.R
@@ -11,12 +11,12 @@ timestamp <- as.POSIXct("2001-8-8 06:30:30")
 spa(timestamp, timezone=3, longitude=33.429859, latitude=35.126413, elevation=135.89, pressure=500, temperature=25, surface.slope=30, surface.azimuth.rotation=-10, DeltaT=64.2)
 
 
-# Example 3: Same as example 2 but without DST (i.e. month February)
+# Example 3: Same as example 2 but without DST (i.e. the timezone is different - month February)
 timestamp <- as.POSIXct("2001-2-8 06:30:30")
-spa(timestamp, timezone=3, longitude=33.429859, latitude=35.126413, elevation=135.89, pressure=500, temperature=25, surface.slope=30, surface.azimuth.rotation=-10, DeltaT=64.2)
+spa(timestamp, timezone=2, longitude=33.429859, latitude=35.126413, elevation=135.89, pressure=500, temperature=25, surface.slope=30, surface.azimuth.rotation=-10, DeltaT=64.2)
 
 
-# Example 4: 
+# Example 4: This example uses a non leap year
 timestamp <- as.POSIXct("1995-3-1 17:20:15")
 spa(timestamp, timezone=2, longitude=-3.7037902, latitude=40.4167754, elevation=647.67, pressure=1000, temperature=19.4, surface.slope=0, surface.azimuth.rotation=180, DeltaT=60.79)
 
